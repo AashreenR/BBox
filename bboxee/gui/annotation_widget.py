407d32fd31b034324b6e0ff8fdcd6c087e75a60e
AashreenR <AashreenR@users.noreply.github.com>
2020-10-06 14:36:29 +0530

    Using Tesserocr Model to create BB

--- bboxee/gui/annotation_widget.py
+++ bboxee/gui/annotation_widget.py
@@ -27,12 +27,16 @@ import sys
 import glob
 import json
 import numpy as np
+import cv2
+import pathlib
 from PIL import Image
+from tesserocr import PyTessBaseAPI, RIL
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from bboxee import schema
 from bboxee.gui import AnnotationAssistant
 from bboxee.gui import AnnotatorDialog
 from bboxee.gui import AnalystDialog
+from collections import defaultdict
 
 if getattr(sys, 'frozen', False):
     bundle_dir = sys._MEIPASS
@@ -644,13 +648,32 @@ class AnnotationWidget(QtWidgets.QWidget, WIDGET):
 
     def load_from_file(self):
         """(Slot) Load existing annotation data from file."""
+        #changed code
         if self.dirty_data_check():
-            file_name = (QtWidgets.
-                         QFileDialog.
-                         getOpenFileName(self,
-                                         'Load Annotations',
-                                         self.image_directory,
-                                         'BBoxEE (*.bbx)'))
+            # file_name = (QtWidgets.
+            #              QFileDialog.
+            #              getOpenFileName(self,
+            #                              'Load Annotations',
+            #                              self.image_directory,
+            #                              'BBoxEE (*.bbx)'))
+            folder_path=self.image_directory
+            images_path = [folder_path + s for s in sorted(os.listdir(folder_path))]
+
+            allboxes = segment_images(images_path, show_segmented_images=False)
+
+            bbx_json = convert_to_bbx(allboxes)
+            file_name= folder_path+'.bbx'
+
+            with open(file_name, 'w') as fp:
+                json.dump(bbx_json, fp)
+            message = ('Bounding boxes file was created as'+file_name +' in the image folder and loaded ')
+            QtWidgets.QMessageBox.warning(self.parent(),
+                                                          'SAVED',
+                                                          message,
+                                                          QtWidgets.QMessageBox.Ok)
+
+
+
             if file_name[0] != '':
                 file = open(file_name[0], 'r')
                 self.data = json.load(file)
@@ -791,6 +814,7 @@ class AnnotationWidget(QtWidgets.QWidget, WIDGET):
                 json.dump(self.data, file, indent=4)
                 file.close()
                 self.set_dirty(False)
+                self.create_images()
                 saved = True
             else:
                 message = ('You are attempting to save the annotations '
@@ -801,6 +825,37 @@ class AnnotationWidget(QtWidgets.QWidget, WIDGET):
                                               message,
                                               QtWidgets.QMessageBox.Ok)
         return saved
+
+    def create_images(self):
+        data_in=self.data
+        folder_path=self.image_directory #insert path of the folder containing images
+        for i in data_in["images"].keys():
+            img_path=folder_path+i
+            thresh1= cv2.imread(img_path,0)
+            # ret,thresh1 = cv2.threshold(im,127,255,cv2.THRESH_BINARY)
+            height, width = thresh1.shape[:2]
+            f=1
+            img_name= i[:-4]
+            new_path=folder_path + img_name
+            if(os.path.isdir(new_path)):
+                files = glob.glob(new_path, recursive=True)
+
+                for file in files:
+                    try:
+                        os.remove(file)
+                    except OSError as e:
+                        print()
+            else:
+                os.mkdir(new_path)
+            for annot in data_in["images"][i]["annotations"]:
+                xmin=int(annot['bbox']["xmin"]*width)
+                xmax=int(annot['bbox']["xmax"]*width)
+                ymin=int(annot['bbox']["ymin"]*height)
+                ymax=int(annot['bbox']["ymax"]*height)
+                new_name = folder_path + img_name + "/" + img_name +"_" + str(f)
+                if (xmax-xmin>22): #to ignore small boxes like pg. nos
+                    cv2.imwrite(new_name+".jpg",thresh1[ymin:ymax,xmin:xmax])
+                    f=f+1
 
     def select_annotator(self):
         self.annotator_selecter.show()
@@ -917,6 +972,157 @@ class AnnotationWidget(QtWidgets.QWidget, WIDGET):
             ann['bbox']['ymax'] = rect.bottom() / self.graphicsView.img_size[1]
             self.update_annotation(ann)
 
+    #modified code starts
+    def segment_images(img_paths, show_segmented_images=True, save_segmented_images=False, save_lines_as_images=False, save_coords = False):
+        allboxes = defaultdict(list)
+        if save_segmented_images is True:
+            pathlib.Path("Results/Pages").mkdir(parents=True, exist_ok=True)
+
+        if save_coords is True:
+            pathlib.Path("Results/Coords").mkdir(parents=True, exist_ok=True)
+
+        with PyTessBaseAPI(lang = 'Devanagari') as api:
+            for page_no, path in enumerate(img_paths):
+
+                if save_lines_as_images is True:
+                    pathlib.Path("Results/Lines/Page" + str(page_no +1)).mkdir(parents=True, exist_ok=True)
+
+                image = Image.open(path)
+                img = np.array(image)
+                image = image.convert('L')
+                api.SetImage(image)
+                boxes = api.GetComponentImages(RIL.TEXTLINE, False)
+
+                # print(boxes)
+                print('Found {} textline image components.'.format(len(boxes)))
+
+                boxlist = []
+
+                for (im, box, _, _) in boxes:
+                    # im is a PIL image object
+                    # box is a dict with x, y, w and h keys
+                    x,y,w,h = box['x'], box['y'], box['w'], box['h']
+
+                    boxlist.append([x,y,w,h])    
+                    # api.SetRectangle(x, y, w, h)
+                    # ocrResult = api.GetUTF8Text()
+                    # conf = api.MeanTextConf()
+                    # print(u"Box[{0}]: x={x}, y={y}, w={w}, h={h}, "
+                    # "confidence: {1}, text: {2}".format(i, conf, ocrResult, **box))
+
+                # Merge Boxes on same line
+
+                boxlist = self.merge_boxes(boxlist)
+
+                coord_dict = {}
+                img2 = img.copy()
+
+                for i, (left, top, right, bottom) in enumerate(boxlist):
+
+                    cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
+                    cv2.putText(img, str(i), (left, top-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+
+                    img_name = path.split("/")[-1]
+                    xmin = left/img.shape[1]
+                    xmax = right/img.shape[1]
+                    ymin = top/img.shape[0]
+                    ymax = bottom/img.shape[0]
+                    allboxes[img_name].append((xmin, xmax, ymin, ymax))
+
+                    if save_lines_as_images is True:
+                        line_img = img2[top:bottom, left:right, :]
+                        filename = 'Results/Lines/Page' + str(page_no +1) +"/" + str(i+1) + ".jpg"
+                        cv2.imwrite(filename, line_img)
+
+                    if save_coords is True:
+                        box_dict = {"top_left": [left, top], 
+                            "top_right":[right, top], 
+                            "bottom_left":[left, bottom],
+                            "bottom_right": [right, bottom]
+                            }
+
+                        coord_dict["box"+str(i+1)] = box_dict
+                
+                if show_segmented_images is True:
+                    scale_percent = 30 # percent of original size
+                    width = int(img.shape[1] * scale_percent / 100)
+                    height = int(img.shape[0] * scale_percent / 100)
+                    dim = (width, height)
+                    # resize image
+                    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+                    cv2_imshow(resized)
+
+                if save_segmented_images is True:
+                    filename = "Results/Pages/" + str(page_no +1) + ".jpg"
+                    cv2.imwrite(filename, img)
+
+                if save_coords is True:
+                    filename = 'Results/Coords/Page' + str(page_no + 1) + '.json'
+                    with open(filename, 'w') as outfile:
+                        json.dump(coord_dict, outfile)
+
+        return allboxes
+
+    def merge_boxes(boxlist):
+        boxlist_np = np.asarray(boxlist)
+        boxlist_np = boxlist_np[boxlist_np[:,1].argsort()] # sort by y
+        x,y,w,h = boxlist_np.T
+        boxlist = np.column_stack((x,y,x+w,y+h)).tolist()
+        median_height = np.median(h) # We filter by half of median height.
+
+        diff = abs(np.ediff1d(y)) # Difference of consecutive elements
+        diff = (diff <= (median_height/2)).nonzero()[0] # Get indices of elements where difference less than avg height/2
+        for i in diff[::-1]: # Go in reverse so that indices are not changed by popping.
+            left = min(x[i], x[i+1])
+            top = min(y[i], y[i+1])
+            right = max(x[i] + w[i], x[i+1] + w[i+1])
+            bottom = max(y[i] + h[i], y[i+1] + h[i+1])
+
+            boxlist[i] = (left,top,right,bottom)
+            boxlist.pop(i+1)
+
+        return boxlist
+
+    def convert_to_bbx(allboxes):
+
+        final_json = {}
+
+        final_json["mask"] = None
+        final_json["mask_name"] = ""
+        final_json["images"] = {}
+
+        for img_name, boxes in allboxes.items():
+            page_dict = {}
+            page_dict["attribution"] = ""
+            page_dict["license"] = ""
+            page_dict["license_url"] = ""
+            annotations = []
+            for box in boxes:
+                box_dict = {}
+                box_dict["created_by"] = "human"
+                box_dict["updated_by"] = "human"
+                box_dict["bbox"] = {
+                    "xmin" : box[0],
+                    "xmax" : box[1],
+                    "ymin" : box[2],
+                    "ymax" : box[3],
+                }
+                box_dict["label"] = "N/A"
+                box_dict["occluded"] = "N"
+                box_dict["truncated"] = "N"
+                box_dict["difficult"] = "N"
+                box_dict["schema"] = "1.0.0"
+
+                annotations.append(box_dict)
+
+            page_dict["annotations"] = annotations
+            final_json["images"][img_name] = page_dict
+
+        final_json["analysts"] = []
+        final_json["schema"] = "1.0.0"
+
+        return final_json
+
     def update_license(self, license):
         if (self.data is not None and
                 self.current_file_name in self.data['images']):
@@ -924,3 +1130,6 @@ class AnnotationWidget(QtWidgets.QWidget, WIDGET):
             rec['attribution'] = license['attribution']
             rec['license'] = license['license']
             rec['license_url'] = license['license_url']
+
+
+
